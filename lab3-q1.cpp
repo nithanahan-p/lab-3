65164f93e1ded26d3cd36a3b3d5a7ca5e710b6c7
nithanahan-p <42238334+nithanahan-p@users.noreply.github.com>
2018-08-17 12:50:59 +0530

    Add files via upload

---
+++ lab3-q1.cpp
@@ -0,0 +1,46 @@
+//including the library
+#include<iostream>
+//write the main function
+using namespace std;
+int main(){
+//declaration of variables of all types
+int i;
+float j;
+char n;
+bool m;
+double k;
+//assigning values to variables
+i=3;
+j=5.3;
+n='d';
+m=1;
+k=2.3012;
+//printing values assigned to variables
+cout<<i<<endl;
+cout<<j<<endl;
+cout<<n<<endl;
+cout<<m<<endl;
+cout<<k<<endl;
+//printing size of variables
+cout<<sizeof(i)<<endl;
+cout<<sizeof(j)<<endl;
+cout<<sizeof(n)<<endl;
+cout<<sizeof(m)<<endl;
+cout<<sizeof(k)<<endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lab3-q2.cpp
@@ -0,0 +1,24 @@
+//including the library
+#include<iostream>
+//write the main function
+using namespace std;
+int main(){
+int i;
+int j;
+int k;
+//assigning values
+i=2;
+j=3;
+k=4;
+//using different operations
+int sum=i+j;
+int sub=k-i;
+int mult=i*j;
+int div=k/i;
+//printing results of operations
+cout<<sum<<endl;
+cout<<sub<<endl;
+cout<<mult<<endl;
+cout<<div<<endl;
+}
+
